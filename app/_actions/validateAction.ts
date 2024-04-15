bb93e054d8930014a49bc73c3d1d165a890e1424
kmkkiii <rskkmkmk@gmail.com>
2024-04-15 17:30:02 +0900

    delete import

--- app/_actions/validateAction.ts
+++ app/_actions/validateAction.ts
@@ -1,7 +1,6 @@
 "use server";
 
 import { exampleFormFactory } from "@/app/_components/exampleFormFactory";
-import { schema } from "@/lib/zod/schema";
 
 export default async function validateAction(
   prev: unknown,
